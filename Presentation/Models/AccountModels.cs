e6f4d2857d3f05a9cb69e901b17c246bfda9b37a
ash400e <sachdevacomputers@yahoo.com>
2013-01-24 23:45:28 +0530

    modified connection string

--- Presentation/Models/AccountModels.cs
+++ Presentation/Models/AccountModels.cs
@@ -11,7 +11,7 @@ namespace Presentation.Models
     public class UsersContext : DbContext
     {
         public UsersContext()
-            : base("DefaultConnection")
+            : base("SQLSERVER_CONNECTION_STRING")
         {
         }
 
